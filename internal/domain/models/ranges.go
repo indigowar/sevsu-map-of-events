8abd6957808961388ce71be260b1c6d622753e2b
Max Kamerov <57366917+indigowar@users.noreply.github.com>
2022-11-13 14:51:47 +0300

    add range models

---
+++ internal/domain/models/ranges.go
@@ -0,0 +1,41 @@
+package models
+
+import "github.com/google/uuid"
+
+type (
+	FoundingRange interface {
+		ID() uuid.UUID
+		Low() int
+		High() int
+	}
+
+	CoFoundingRange interface {
+		FoundingRange
+	}
+
+	foundingRange struct {
+		id   uuid.UUID
+		low  int
+		high int
+	}
+)
+
+func (f foundingRange) ID() uuid.UUID {
+	return f.id
+}
+
+func (f foundingRange) Low() int {
+	return f.low
+}
+
+func (f foundingRange) High() int {
+	return f.high
+}
+
+func NewRange(low int, high int) FoundingRange {
+	return &foundingRange{
+		id:   uuid.New(),
+		low:  low,
+		high: high,
+	}
+}
