1b65335e3ea98a87cfb3c29ed4be67739296c404
Max Kamerov <57366917+indigowar@users.noreply.github.com>
2022-11-13 16:41:23 +0300

    Added validators for ranges.

---
+++ internal/domain/validators/range.go
@@ -0,0 +1,43 @@
+package validators
+
+import (
+	"errors"
+
+	"github.com/indigowar/map-of-events/internal/domain/models"
+)
+
+var (
+	ErrLowBiggerThenHighValue      = errors.New("range's low value is bigger then high value")
+	ErrRangeHasNegativeValues      = errors.New("range has negative value(s)")
+	ErrRangeHasUnacceptablePercent = errors.New("range has a value of unaccepted percent")
+)
+
+func ValidateRange(r models.FoundingRange) error {
+	high := r.High()
+	low := r.Low()
+
+	if high > low {
+		return ErrLowBiggerThenHighValue
+	}
+
+	if low < 0 || high < 0 {
+		return ErrRangeHasNegativeValues
+	}
+
+	return nil
+}
+
+func ValidatePercentRange(r models.CoFoundingRange) error {
+	high := r.High()
+	low := r.Low()
+
+	if err := ValidateRange(r); err != nil {
+		return err
+	}
+
+	if high > 100 || low > 100 {
+		return ErrRangeHasUnacceptablePercent
+	}
+
+	return nil
+}
