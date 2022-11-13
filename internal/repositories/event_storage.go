db5015f318db772f79fae92a9e08b71cfe19044b
Max Kamerov <57366917+indigowar@users.noreply.github.com>
2022-11-13 18:47:41 +0300

    Wrote the user storage repository

---
+++ internal/repositories/event_storage.go
@@ -0,0 +1,18 @@
+package repositories
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+
+	"github.com/indigowar/map-of-events/internal/domain/models"
+)
+
+type EventStorageRepository interface {
+	GetIDList(ctx context.Context) ([]uuid.UUID, error)
+	Filter(ctx context.Context) ([]uuid.UUID, error)
+	GetByID(ctx context.Context, id uuid.UUID) (models.Event, error)
+	Create(ctx context.Context, event models.Event) error
+	Delete(ctx context.Context, id uuid.UUID) error
+	Update(ctx context.Context, event models.Event) error
+}
