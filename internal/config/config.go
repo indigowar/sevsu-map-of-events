93ccac6cc672a16a01b1d302415a91daa5c443f8
Max Kamerov <57366917+indigowar@users.noreply.github.com>
2022-11-13 11:22:11 +0300

    Added loading of http configuration

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,17 +1,34 @@
 package config
 
-import "os"
+import (
+	"github.com/spf13/viper"
+	"os"
+	"time"
+)
+
+const (
+	defaultHTTPPort               = "8000"
+	defaultHTTPRWTimeout          = 10 * time.Second
+	defaultHTTPMaxHeaderMegaBytes = 1
+
+	envLocal = "local"
+)
 
 type (
 	HTTPConfig struct {
+		Port                string        `mapstructure:"port"`
+		ReadTimeout         time.Duration `mapstructure:"readTimeout"`
+		WriteTimeout        time.Duration `mapstructure:"writeTimeout"`
+		MaxHeadersMegabytes int           `mapstructure:"maxHeaderMegaBytes"`
 	}
 
 	PostgresConfig struct {
 	}
 
 	Config struct {
-		HTTP     HTTPConfig
-		Postgres PostgresConfig
+		HTTP        HTTPConfig
+		Postgres    PostgresConfig
+		Environment string
 	}
 )
 
@@ -34,17 +51,37 @@ func Init(configDir string) (*Config, error) {
 }
 
 func populateDefaults() {
-
+	viper.SetDefault("http.port", defaultHTTPPort)
+	viper.SetDefault("http.timeouts.write", defaultHTTPRWTimeout)
+	viper.SetDefault("http.timeouts.read", defaultHTTPRWTimeout)
+	viper.SetDefault("http.max_header_megabytes", defaultHTTPMaxHeaderMegaBytes)
 }
 
 func parseConfigFile(dir string, env string) error {
-	return nil
+	viper.AddConfigPath(dir)
+	viper.SetConfigName("main")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return err
+	}
+
+	if env == envLocal {
+		return nil
+	}
+
+	viper.SetConfigName(env)
+
+	return viper.MergeInConfig()
 }
 
 func unmarshal(c *Config) error {
+	if err := viper.UnmarshalKey("http", &c.HTTP); err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func setFromEnv(c *Config) {
-
+	c.Environment = os.Getenv("ENV")
 }
