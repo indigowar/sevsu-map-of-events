bbf7e2c362078f11fa08b4693365839f538dc36c
Max Kamerov <57366917+indigowar@users.noreply.github.com>
2022-11-13 18:52:08 +0300

    Wrote the RangeStorageRepository

---
+++ internal/repositories/range_storage.go
@@ -0,0 +1,16 @@
+package repositories
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+
+	"github.com/indigowar/map-of-events/internal/domain/models"
+)
+
+type RangeStorageRepository interface {
+	GetByID(ctx context.Context, id uuid.UUID) (models.FoundingRange, error)
+	GetMaximumRange(ctx context.Context) (int, int, error)
+	Create(ctx context.Context, foundingRange models.FoundingRange) error
+	Delete(ctx context.Context, id uuid.UUID) error
+}
