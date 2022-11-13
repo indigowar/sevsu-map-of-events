1b9f8cef0f90edbdf38e21f803d48a749f98d8f6
Max Kamerov <57366917+indigowar@users.noreply.github.com>
2022-11-13 11:11:59 +0300

    Rewrite main

--- cmd/eventmap/main.go
+++ cmd/eventmap/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"github.com/indigowar/map-of-events/internal/app"
 	"github.com/indigowar/map-of-events/internal/config"
+	"log"
 )
 
 func main() {
-	cfg := config.LoadConfig()
+	cfg, err := config.Init("config")
+	if err != nil {
+		log.Fatalln("Wrong configuration of application ", err)
+	}
 	app.Run(cfg)
 }
