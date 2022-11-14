a87b177e664a63e6ab74f2a002d6007f20d71b40
Max Kamerov <57366917+indigowar@users.noreply.github.com>
2022-11-14 16:39:43 +0300

    Add gin handlers for retrieving and creating new competitors

---
+++ internal/delivery/http/json/competitor.go
@@ -0,0 +1,61 @@
+package json
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+
+	"github.com/indigowar/map-of-events/internal/domain/services"
+)
+
+type Competitor struct {
+	Id             uuid.UUID `json:"id"`
+	CompetitorName string    `json:"name"`
+}
+
+func GetAllCompetitorsHandler(svc services.CompetitorService) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		comps, err := svc.GetAll(c)
+
+		if err != nil {
+			log.Println(err)
+			c.Status(http.StatusInternalServerError)
+		}
+
+		result := make([]Competitor, len(comps))
+		for i, v := range comps {
+			result[i] = Competitor{v.ID(), v.Name()}
+		}
+
+		c.JSON(http.StatusOK, result)
+	}
+}
+
+func CreateCompetitorHandler(srv services.CompetitorService) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		var name string
+		if err := c.ShouldBindJSON(&name); err != nil {
+			log.Println(err)
+			c.Status(http.StatusBadRequest)
+			return
+		}
+		obj, err := srv.Create(c, name)
+		if err != nil {
+			log.Println(err)
+			c.Status(http.StatusInternalServerError)
+			return
+		}
+
+		c.JSON(http.StatusAccepted, Competitor{obj.ID(), obj.Name()})
+	}
+}
+
+func (c *Competitor) ID() uuid.UUID {
+	return c.Id
+}
+
+func (c *Competitor) Name() string {
+	return c.CompetitorName
+}
