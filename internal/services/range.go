5147a36f38020019c0689e4e1a8751ed0733313c
Max Kamerov <57366917+indigowar@users.noreply.github.com>
2022-11-16 13:23:03 +0300

    Refactor Range Service

--- internal/services/range.go
+++ internal/services/range.go
@@ -15,34 +15,34 @@ import (
 
 type rangeService struct {
 	storage        storages.RangeStorageRepository
-	validatorsList []func(foundingRange models.Range) error
+	validatorsList []func(foundingRange models.RangeModel) error
 }
 
-func (svc rangeService) GetByID(ctx context.Context, id uuid.UUID) (models.Range, error) {
+func (svc rangeService) GetByID(ctx context.Context, id uuid.UUID) (models.RangeModel, error) {
 	r, err := svc.storage.GetByID(ctx, id)
 	if err != nil {
 		log.Println(err)
-		return nil, errors.New("internal error")
+		return models.RangeModel{}, errors.New("internal error")
 	}
 
 	return r, nil
 }
 
-func (svc rangeService) GetMaximumRange(ctx context.Context) (models.Range, error) {
+func (svc rangeService) GetMaximumRange(ctx context.Context) (models.RangeModel, error) {
 	result, err := svc.storage.GetMaximumRange(ctx)
 	if err != nil {
 		log.Println(err)
-		return nil, errors.New("internal error")
+		return models.RangeModel{}, errors.New("internal error")
 	}
 	return result, nil
 }
 
-func (svc rangeService) Create(ctx context.Context, low, high int) (models.Range, error) {
-	r := models.NewRange(uuid.New(), low, high)
+func (svc rangeService) Create(ctx context.Context, low, high int) (models.RangeModel, error) {
+	r := models.RangeModel{ID: uuid.New(), Low: low, High: high}
 
 	for _, validator := range svc.validatorsList {
 		if err := validator(r); err != nil {
-			return nil, err
+			return models.RangeModel{}, err
 		}
 	}
 
@@ -53,16 +53,16 @@ func (svc rangeService) Delete(ctx context.Context, id uuid.UUID) error {
 	return svc.Delete(ctx, id)
 }
 
-func (svc rangeService) Update(ctx context.Context, foundingRange models.Range) (models.Range, error) {
+func (svc rangeService) Update(ctx context.Context, foundingRange models.RangeModel) (models.RangeModel, error) {
 	for _, validator := range svc.validatorsList {
 		if err := validator(foundingRange); err != nil {
-			return nil, err
+			return models.RangeModel{}, err
 		}
 	}
 
-	err := svc.Delete(ctx, foundingRange.ID())
+	err := svc.Delete(ctx, foundingRange.ID)
 	if err != nil {
-		return nil, err
+		return models.RangeModel{}, err
 	}
 	return svc.storage.Create(ctx, foundingRange)
 }
@@ -70,14 +70,14 @@ func (svc rangeService) Update(ctx context.Context, foundingRange models.Range) 
 func NewFoundingRangeService(storage storages.RangeStorageRepository) services.RangeService {
 	return &rangeService{
 		storage:        storage,
-		validatorsList: []func(foundingRange models.Range) error{validators.ValidateRange},
+		validatorsList: []func(foundingRange models.RangeModel) error{validators.ValidateRange},
 	}
 }
 
 func NewCoFoundingRangeService(storage storages.RangeStorageRepository) services.RangeService {
 	return &rangeService{
 		storage: storage,
-		validatorsList: []func(foundingRange models.Range) error{
+		validatorsList: []func(foundingRange models.RangeModel) error{
 			validators.ValidateRange,
 			validators.ValidatePercentRange,
 		},
