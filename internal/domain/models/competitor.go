3244b339fbb9adbebfac7d0ecf5d756e9030edc2
Max Kamerov <57366917+indigowar@users.noreply.github.com>
2022-11-16 12:05:01 +0300

    Reduced count of files, moved all models in one file.

--- internal/domain/models/competitor.go
+++
@@ -1,8 +0,0 @@
-package models
-
-import "github.com/google/uuid"
-
-type Competitor struct {
-	ID   uuid.UUID
-	Name string
-}
--- internal/domain/models/organizer_level.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "github.com/google/uuid"
-
-type OrganizerLevel struct {
-	ID   uuid.UUID
-	Name string
-	Code string
-}
