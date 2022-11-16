d9228caa07fb05e9cb63aa93f5380c459db8fc61
Max Kamerov <57366917+indigowar@users.noreply.github.com>
2022-11-16 12:05:06 +0300

    Reduced count of files, moved all models in one file.

---
+++ internal/domain/models/models.go
@@ -0,0 +1,53 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type OrganizerLevel struct {
+	ID   uuid.UUID
+	Name string
+	Code string
+}
+
+type Organizer struct {
+	ID    uuid.UUID
+	Name  string
+	Logo  string
+	Level uuid.UUID
+}
+
+type RangeModel struct {
+	ID   uuid.UUID
+	Low  int
+	High int
+}
+
+type FoundingRange = RangeModel
+type CoFoundingRange = RangeModel
+
+type Competitor struct {
+	ID   uuid.UUID
+	Name string
+}
+
+type Event struct {
+	ID                  uuid.UUID
+	Title               string
+	Organizer           uuid.UUID
+	FoundingType        string
+	FoundingRange       uuid.UUID
+	CoFoundingRange     uuid.UUID
+	SubmissionDeadline  time.Time
+	ConsiderationPeriod string
+	RealisationPeriod   string
+	Result              string
+	Site                string
+	Document            string
+	InternalContacts    string
+	TRL                 int
+	Competitors         []uuid.UUID
+	Subjects            []uuid.UUID
+}
