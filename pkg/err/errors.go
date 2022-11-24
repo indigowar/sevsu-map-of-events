4f42ec42cbf2c7bd6bd354ca99a0ca45440d160e
Max Kamerov <57366917+indigowar@users.noreply.github.com>
2022-11-24 19:45:48 +0300

    add error impl

--- pkg/err/errors.go
+++ pkg/err/errors.go
@@ -5,3 +5,29 @@ type Error interface {
 	ShortErr() string
 	LongErr() string
 }
+
+type errorType struct {
+	reason int
+	short  string
+	long   string
+}
+
+func (e errorType) Reason() int {
+	return e.reason
+}
+
+func (e errorType) ShortErr() string {
+	return e.short
+}
+
+func (e errorType) LongErr() string {
+	return e.long
+}
+
+func New(reason int, short, long string) Error {
+	return &errorType{
+		reason: reason,
+		short:  short,
+		long:   long,
+	}
+}
